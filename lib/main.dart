ea9fdfb5ef0f87846a7c1cdb69237c558b169be5
MarcosAndreLS <marcosofc04@gmail.com>
2025-04-04 12:51:42 -0300

    Adicionando snackBar e desfazendo ações

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,8 @@ class _HomeState extends State<Home> {
   final _toDoController = TextEditingController();
 
   List _toDoList = [];
+  Map<String, dynamic>? _lastRemoved;
+  int? _lastRemovedPos;
 
   @override
   void initState() {
@@ -124,6 +126,35 @@ class _HomeState extends State<Home> {
           });
         },
       ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text(
+              "Tarefa \"${_lastRemoved!["title"]}\" removida!",
+            ),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(
+                    _lastRemovedPos!,
+                    _lastRemoved,
+                  );
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 4),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
